af08bc53b9d863774e194dd00486735d9125d078
thasuohm <thasuohm@gmail.com>
2023-02-05 10:13:42 +0700

    update contact

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import gmail from '/images/gmail.png'
 import github from '/images/github.png'
+import tel from '/images/tel.jpg'
 import profileImage from '/images/profile.jpg'
 import {a} from '@react-spring/web'
 import useBooping from '../hooks/useBooping'
@@ -36,13 +37,18 @@ const AboutMe = () => {
             I'am in love with Coding and find the way to make my code even
             Better!
           </p>
-          <div className="flex gap-6 justify-center sm:justify-start items-baseline mt-4">
+          <div className="flex gap-6 justify-center sm:justify-start items-center mt-4">
             <a
               href="https://github.com/thasuohm"
               target="_blank"
               className="w-10"
             >
-              <img src={github} alt="thasuohm" title="thasuohm" />
+              <img
+                src={github}
+                alt="thasuohm"
+                title="thasuohm"
+                className="rounded-full"
+              />
             </a>
             <a
               href="mailto:thanapat.suwanaaard@gmail.com"
@@ -55,6 +61,16 @@ const AboutMe = () => {
                 title="thanapat.suwanaaard@gmail.com"
               />
             </a>
+
+            <div className="flex gap-2 sm:gap-3 items-center">
+              <img
+                src={tel}
+                alt="thanapat.suwanaaard@gmail.com"
+                title="thanapat.suwanaaard@gmail.com"
+                className="w-10 rounded-full"
+              />
+              <span className="text-lg sm:text-xl font-bold font-menlo">061-896-9299</span>
+            </div>
           </div>
         </a.div>
         <a.div
